503260d417d155133a5434c46f6b45f49b16496d
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-12-28 09:00:08 +0100

    Add create and new method in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save!
+      redirect_to myportfolios_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:photo, :title, :description, :url, :user_id, :myportfolio_id)
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,7 @@ class Project < ApplicationRecord
   belongs_to :myportfolio
   has_one_attached :photo
 
+  validates :title, presence: true
+  validates :photo, presence: true
+  validates :url, presence: true
 end
