ee5fa28a647ce23a5f79d6a100aec9d3021f514e
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-12-28 10:37:07 +0100

    Add edit and update method to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:edit, :update]
+
   def new
     @project = Project.new
   end
@@ -13,9 +15,24 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to myportfolios_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def project_params
     params.require(:project).permit(:photo, :title, :description, :url)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
