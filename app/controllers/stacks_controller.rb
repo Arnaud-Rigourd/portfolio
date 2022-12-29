8686a974700c4e4de2d87c4ae75fde81bc65b159
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-12-29 17:26:12 +0100

    edit, update, destroy method done for stack

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -1,5 +1,5 @@
 class StacksController < ApplicationController
-
+  before_action :set_stack, only: [:edit, :update, :destroy]
 
   def new
     @stack = Stack.new
@@ -16,9 +16,29 @@ class StacksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @stack.update(stack_params)
+      redirect_to myportfolios_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @stack.destroy
+    redirect_to myportfolios_path
+  end
+
   private
 
   def stack_params
     params.require(:stack).permit(:name, :photo, :user_id)
+  end
+
+  def set_stack
+    @stack = Stack.find(params[:id])
   end
 end
