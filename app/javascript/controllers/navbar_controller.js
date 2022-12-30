de726d0b19b13ec7cb89de35b5d833bef9b39ad6
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-12-30 15:31:24 +0100

    Fix navbar issue on scroll

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,18 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["navbar", "header", "wrapper"]
+  static targets = ["navbar", "wrapper"]
 
   connect() {
     // console.log(`scrollY = ${window.scrollY}`)
-    console.log(`scrollY = ${this.wrapperTarget.scrollY}`)
-    console.log(window.innerHeight)
-    console.log(this.headerTarget.innerHeight)
+    // console.log(`scrollY = ${this.wrapperTarget.scrollTop}`)
+    // console.log(window.innerHeight)
+    // console.log(this.headerTarget.innerHeight)
   }
 
   updateNavbar() {
-    console.log(`scrollY = ${this.wrapperTarget.scrollTop}`)
-
+    // console.log(`scrollY = ${this.wrapperTarget.scrollTop}`)
     if (this.wrapperTarget.scrollTop >= window.innerHeight) {
       this.navbarTarget.classList.add("nav-bg")
     } else {
