c1dece24a50c8d4d2038ec93e7f8f9ff5a120c6b
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-01-02 15:09:06 +0100

    Add responsive

--- app/controllers/myportfolios_controller.rb
+++ app/controllers/myportfolios_controller.rb
@@ -1,15 +1,18 @@
 class MyportfoliosController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
+    @projects = Project.all
+    @stacks = Stack.all
+
     if user_signed_in?
       @user = current_user
+      @projects = @user.projects.order(:position)
+      @stacks = @user.stacks.order(:position)
+    else
+      @projects = @projects.order(:position)
+      @stacks = @stacks.order(:position)
     end
 
-    @projects = Project.all
-    @projects = @user.projects.order(:position)
-
-    @stacks = Stack.all
-    @stacks = @user.stacks.order(:position)
   end
 
   def carousel
