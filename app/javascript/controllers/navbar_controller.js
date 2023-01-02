007df3fdd6f3ce67877d01faaa4a80d7c389bf36
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-01-02 17:09:15 +0100

    Add burfer menu and full responsive media queries

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,10 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["navbar", "wrapper", "dropdownList"]
+  static targets = ["navbar", "wrapper", "dropdownList", "toggleDiv"]
 
-  // connect() {
-  // }
+  connect() {
+    if (window.innerWidth <= 576) {
+      this.toggleDivTarget.classList.remove('hidden')
+    } else {
+      this.toggleDivTarget.classList.add('hidden')
+    }
+
+    window.addEventListener("resize", (event) => {
+      if (window.innerWidth <= 576) {
+        this.toggleDivTarget.classList.remove('hidden')
+      } else {
+        this.toggleDivTarget.classList.add('hidden')
+      }
+    })
+  }
 
   updateNavbar() {
     if (this.wrapperTarget.scrollTop >= (window.innerHeight - 66)) {
