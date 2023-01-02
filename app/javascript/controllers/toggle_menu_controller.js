b19688c03adcf8613a3a1aeffa090bca518ea053
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-01-02 19:40:32 +0100

    Add function to close dropdown when click outside dropdown list

--- app/javascript/controllers/toggle_menu_controller.js
+++ app/javascript/controllers/toggle_menu_controller.js
@@ -4,12 +4,25 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["burgerMenu", "dropdownList"]
 
-  connect() {
-  }
+  // connect() {
+  // }
 
-  toggleAction() {
+  toggleAction(event) {
     this.dropdownListTarget.classList.toggle('hidden')
     this.burgerMenuTarget.classList.toggle('fa-bars')
     this.burgerMenuTarget.classList.toggle('fa-xmark')
+    event.stopPropagation()
+
+    document.addEventListener('click', (e) => {
+      if (this.dropdownListTarget.matches('.hidden')) {
+        e.stopPropagation()
+        return
+      } else {
+        this.dropdownListTarget.classList.toggle('hidden')
+        this.burgerMenuTarget.classList.toggle('fa-bars')
+        this.burgerMenuTarget.classList.toggle('fa-xmark')
+        e.stopPropagation()
+      }
+    })
   }
 }
