e7784657fb72d723ceceb34cd829e205e60da425
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-01-02 16:03:17 +0100

    Add burger menu and set up data controller to toggle menu

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["navbar", "wrapper"]
+  static targets = ["navbar", "wrapper", "dropdownList"]
 
   // connect() {
   // }
@@ -9,8 +9,10 @@ export default class extends Controller {
   updateNavbar() {
     if (this.wrapperTarget.scrollTop >= (window.innerHeight - 66)) {
       this.navbarTarget.classList.add("nav-bg")
+      this.dropdownListTarget.classList.add("nav-bg")
     } else {
       this.navbarTarget.classList.remove("nav-bg")
+      this.dropdownListTarget.classList.remove("nav-bg")
     }
   }
 }
